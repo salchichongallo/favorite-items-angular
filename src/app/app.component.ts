c5be6ef56bf62a8e82e077019cc5e123debc49ab
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-08-13 12:49:56 -0500

    chore: remove unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <h1>App</h1> `,
 })
-export class AppComponent {
-  title = 'favorite-items-angular';
-}
+export class AppComponent {}
